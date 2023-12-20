773247c3a027094ac43e84b8ab3052ae7b2f5b96
Universe0809 <minheriksen@gmail.com>
2023-12-20 05:01:22 -0500

    feat(vue): add interactive functionality

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-Vue.createApp({}).mount("#app");
+const vm = Vue.createApp({
+  data() {
+    return {
+      firstName: "Minh",
+      lastName: "Eriksen",
+      url: "https://google.com",
+      age: 23,
+      purple: true,
+    };
+  },
+  methods: {
+    fullName() {
+      return `${this.firstName} ${this.lastName.toUpperCase()}`;
+    },
+    increment() {
+      this.age++;
+    },
+  },
+  computed: {
+    circle_classes() {
+      return { purple: this.purple };
+    },
+  },
+}).mount("#app");
